a93d27266233e3276254333a4bc7da7670db6277
LuthGom <lucianomendes23@yahoo.com>
2022-01-25 13:02:49 -0300

    Correcting style

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ function GlobalStyle() {
   );
 }
 function Titulo(props) {
-  const Tag = props.tag || "h1";
+  const Tag = props.tag || 'h1';
   return (
     <>
       <Tag>{props.children}</Tag>
@@ -48,38 +48,32 @@ function Titulo(props) {
   );
 }
 export default function PaginaInicial() {
-  const username = "LuthGom";
+  const username = 'LuthGom';
+  
 
   return (
     <>
+    <GlobalStyle />
       <Box
         styleSheet={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          display: 'flex', alignItems: 'center', justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage:
-            "url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundBlendMode: "multiply",
+          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
         <Box
           styleSheet={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
             flexDirection: {
-              xs: "column",
-              sm: "row",
+              xs: 'column',
+              sm: 'row',
             },
-            width: "100%",
-            maxWidth: "700px",
-            borderRadius: "5px",
-            padding: "32px",
-            margin: "16px",
-            boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+            width: '100%', maxWidth: '700px',
+            borderRadius: '5px', padding: '32px', margin: '16px',
+            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
             backgroundColor: appConfig.theme.colors.neutrals[700],
           }}
         >
@@ -88,36 +82,40 @@ export default function PaginaInicial() {
             as="form"
             onSubmit={function (infosDoEvento) {
               infosDoEvento.preventDefault();
-              console.log("Alguém submeteu o form");
-              roteamento.push("/chat");
+              console.log('Alguém submeteu o form');
+              roteamento.push('/chat');
+              // window.location.href = '/chat';
             }}
             styleSheet={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              width: { xs: "100%", sm: "50%" },
-              textAlign: "center",
-              marginBottom: "32px",
+              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-            <Text
-              variant="body3"
-              styleSheet={{
-                marginBottom: "32px",
-                color: appConfig.theme.colors.neutrals[300],
-              }}
-            >
+            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
 
+            {/* <input
+                            type="text"
+                            value={username}
+                            onChange={function (event) {
+                                console.log('usuario digitou', event.target.value);
+                                // Onde ta o valor?
+                                const valor = event.target.value;
+                                // Trocar o valor da variavel
+                                // através do React e avise quem precisa
+                                setUsername(valor);
+                            }}
+                        /> */}
             <TextField
               value={username}
               onChange={function (event) {
-                console.log("usuario digitou", event.target.value);
+                console.log('usuario digitou', event.target.value);
+                // Onde ta o valor?
                 const valor = event.target.value;
-
+                // Trocar o valor da variavel
+                // através do React e avise quem precisa
                 setUsername(valor);
               }}
               fullWidth
@@ -131,8 +129,8 @@ export default function PaginaInicial() {
               }}
             />
             <Button
-              type="submit"
-              label="Entrar"
+              type='submit'
+              label='Entrar'
               fullWidth
               buttonColors={{
                 contrastColor: appConfig.theme.colors.neutrals["000"],
@@ -144,26 +142,27 @@ export default function PaginaInicial() {
           </Box>
           {/* Formulário */}
 
+
           {/* Photo Area */}
           <Box
             styleSheet={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              maxWidth: "200px",
-              padding: "16px",
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              maxWidth: '200px',
+              padding: '16px',
               backgroundColor: appConfig.theme.colors.neutrals[800],
-              border: "1px solid",
+              border: '1px solid',
               borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: "10px",
+              borderRadius: '10px',
               flex: 1,
-              minHeight: "240px",
+              minHeight: '240px',
             }}
           >
             <Image
               styleSheet={{
-                borderRadius: "50%",
-                marginBottom: "16px",
+                borderRadius: '50%',
+                marginBottom: '16px',
               }}
               src={`https://github.com/${username}.png`}
             />
@@ -172,8 +171,8 @@ export default function PaginaInicial() {
               styleSheet={{
                 color: appConfig.theme.colors.neutrals[200],
                 backgroundColor: appConfig.theme.colors.neutrals[900],
-                padding: "3px 10px",
-                borderRadius: "1000px",
+                padding: '3px 10px',
+                borderRadius: '1000px'
               }}
             >
               {username}
