4c6b8ff379ac4c4bd9edcac9ed441a0834267625
LuthGom <lucianomendes23@yahoo.com>
2022-01-28 19:53:48 -0300

    Implementing live chat

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ function Titulo(props) {
 
 export default function PaginaInicial() {
   const [userName, setUserName] = useState("");
-  const [obj, setObj] = useState("");
+  const [obj, setObj] = useState();
   const roteamento = useRouter();
   useEffect(() => {
     const profileGit = async function () {
@@ -75,7 +75,6 @@ export default function PaginaInicial() {
             as="form"
             onSubmit={function (infosDoEvento) {
               infosDoEvento.preventDefault();
-              console.log("Alguém submeteu o form");
               roteamento.push(`/chat?username=${userName}`);
             }}
             styleSheet={{
@@ -101,7 +100,6 @@ export default function PaginaInicial() {
 
             <TextField
               onChange={function (event) {
-                console.log("usuario digitou", event.target.value);
                 const valor = event.target.value;
                 setUserName(valor);
               }}
